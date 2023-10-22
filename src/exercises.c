6dc204cb5d8751ee2ddf69422687863e6aaf65dd
talhaRehmann <kingtalha312@gmail.com>
2023-10-22 23:09:29 +0500

    q3 exercise.c

--- src/exercises.c
+++ src/exercises.c
@@ -52,6 +52,17 @@ int main(int argc, char **argv) {
 
   //3. Print numbers 1 thru n separated by a comma except for
   //   the last one; example: 1, 2, 3, 4, 5
+      
+      printf("enter the value for n");
+  scanf("%d",&n);
+for (int i = 1; i <= n; i++) {
+    if (i == n) {
+    printf("%d", i);  
+      } else {
+   printf("%d, ", i);  
+  }
+}
+    printf("\n");
 
   //4. Print squares of numbers 1 thru n separated by a comma
   //   except for the last one; example: 1, 4, 9, 16, 25
